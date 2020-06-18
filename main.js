137b22d470d69b29a1e04de9c2b511aff9d9b9c9
kengo0629 <63942635+kengo0629@users.noreply.github.com>
2020-06-19 08:38:23 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
-var ary = [2,4,6,8,10];
-var sum = 0;
 
-for (let i = 0; i < 5; i++){
-  sum += ary[i];
+
+for(let i = 1; i <= 10; i++){
+  if(i == 3){
+    continue;
+  }
+  if(i == 7){
+    break;
+  }
+  console.log(i);
 }
-
-console.log(sum);
